8f7a84b88451c2cb7882e7f43cb43d60e2f85a2d
hejh <hejh@akulaku.com>
2024-04-10 15:37:31 +0800

    init

---
+++ mybatis-plus-code-generator/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @Author: hejh
+ * @Date: 2024/4/10 15:32
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
